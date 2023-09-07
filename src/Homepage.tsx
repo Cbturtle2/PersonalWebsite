ec4a449860d0673bb635d4af00e42501562bb054
Charles Beam <charleslbeam@gmail.com>
2023-09-07 12:35:04 -0500

    Updated Experience

--- src/Homepage.tsx
+++ src/Homepage.tsx
@@ -35,55 +35,60 @@ const Homepage = () => {
                 </div>
                 <div className="flex flex-col justify-start sm:text-left text-center gap-5">
                     <h2>Experience</h2>
-                    <h4>For a more detailed description of each position just click the corresponding experience</h4>
+                    <h4>
+                        For a more detailed description of each position please see{" "}
+                        <a href="/resume" className="text-blue-200">
+                            Resume
+                        </a>
+                    </h4>
                     <div className="flex flex-col gap-6">
                         <Experience
-                            image="https://s3.csh.rit.edu/lontronix-website/companies/bryx.jpg"
+                            image="https://pub-4a5c351e48c34a18817678ead6714bda.r2.dev/BryxLogo.jpeg"
                             title="Bryx"
                             jobTitle="Full Time Web Developer"
-                            dates="September 2022 - June 2023"
+                            dates="Sept 2022 - June 2023"
                             description="Brought a Records Management System from conception to beta testing. Apart of team that built internal UI library from scratch."
                         />
                         <Experience
-                            image="https://s3.csh.rit.edu/lontronix-website/companies/bryx.jpg"
+                            image="https://pub-4a5c351e48c34a18817678ead6714bda.r2.dev/BryxLogo.jpeg"
                             title="Bryx"
                             jobTitle="Summer Intern Web Developer"
-                            dates="May 2022 - August 2022"
+                            dates="May 2022 - Aug 2022"
                             description="Planned Records Management System and built internal systems to support it. Apart of hiring effort to grow and onboard team."
                         />
                         <Experience
-                            image="https://s3.csh.rit.edu/lontronix-website/companies/bryx.jpg"
+                            image="https://pub-4a5c351e48c34a18817678ead6714bda.r2.dev/BryxLogo.jpeg"
                             title="Bryx"
                             jobTitle="Part Time Data Scientist"
-                            dates="February 2022 - May 2022"
+                            dates="Feb 2022 - May 2022"
                             description="Identified trends from past support ticket/phone records and collaborated with support team to improve processes. Developed system to generate PDFs for company badges."
                         />
                         <Experience
-                            image="https://assets.csh.rit.edu/pubsite/csh_logo_square.svg"
+                            image="https://pub-4a5c351e48c34a18817678ead6714bda.r2.dev/CSH Logo.svg"
                             title="Computer Science House"
                             jobTitle="Executive Board Member"
-                            dates="September 2021 - May 2022"
+                            dates="Sept 2021 - May 2022"
                             description="On Executive Board managing organization with 100+ members. Responsible for hosting weekly meetings and planning social events."
                         />
                         <Experience
-                            image="https://s3.csh.rit.edu/lontronix-website/companies/bryx.jpg"
+                            image="https://pub-4a5c351e48c34a18817678ead6714bda.r2.dev/RIT Logo.jpeg"
                             title="Rochester Institute of Technology"
                             jobTitle="Ambassador"
-                            dates="September 2021 - May 2022"
+                            dates="Sept 2021 - May 2022"
                             description="Worked events with 100s of prospective students and their families. Communicated with Computer Science Department heads on general student opinions/concerns"
                         />
                         <Experience
-                            image="https://s3.csh.rit.edu/lontronix-website/companies/bryx.jpg"
+                            image="https://pub-4a5c351e48c34a18817678ead6714bda.r2.dev/NSU Logo.png"
                             title="Louisiana Aerospace Catalyst Experiences for Students"
                             jobTitle="Programming Manager and Embedded Software Developer"
-                            dates="August 2020 - May 2021"
+                            dates="Aug 2020 - May 2021"
                             description="In charge of coordinating programming of payload to be launched into upper atmosphere (100,000+ ft). Developed software to control payload and collect data from sensors. Coordinated with team members to ensure all aspects of project were completed on time and tested appropriately."
                         />
                         <Experience
-                            image="https://s3.csh.rit.edu/lontronix-website/companies/bryx.jpg"
+                            image="https://pub-4a5c351e48c34a18817678ead6714bda.r2.dev/NSU Logo.png"
                             title="Louisiana Aerospace Catalyst Experiences for Students"
                             jobTitle="Embedded Software Developer"
-                            dates="December 2019 - May 2020"
+                            dates="Dec 2019 - May 2020"
                             description="Worked with team members to develop software for custom board to control payload and collect data"
                         />
                     </div>
--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -8,23 +8,21 @@ interface ExperienceProps extends React.HTMLAttributes<HTMLButtonElement> {
     description: string;
 }
 
-const Experience = ({ image, title, jobTitle, dates, description, ...props }: ExperienceProps) => {
+const Experience = ({ image, title, jobTitle, dates, description }: ExperienceProps) => {
     return (
-        <Link to={`experience/${title.toLowerCase().replace(/\s/g, "-")}`}>
-            <button className="flex gap-3 ml-3 w-full" {...props}>
-                <img src={image} className="rounded-lg w-16 h-16" />
-                <div className="flex flex-col items-start w-full">
-                    <div className="flex justify-between w-full">
-                        <h3 className="font-bold">{title}</h3>
-                        <h2 className="font-bold">|</h2>
-                        <h4 className="font-bold">{jobTitle}</h4>
-                        <h2 className="font-bold">|</h2>
-                        <h4 className="font-bold">{dates}</h4>
-                    </div>
-                    <p className="sm:text-left text-center w-full">{description}</p>
+        <div className="flex gap-3 ml-3 w-full mb-8">
+            <img src={image} className="rounded-lg w-16 h-16" />
+            <div className="flex flex-col items-start w-full">
+                <div className="flex justify-between w-full">
+                    <h3 className="font-bold w-1/3 text-center">{title}</h3>
+                    <h2 className="font-bold">|</h2>
+                    <h4 className="font-bold w-1/3 text-center">{jobTitle}</h4>
+                    <h2 className="font-bold">|</h2>
+                    <h4 className="font-bold 1-1/3 text-center">{dates}</h4>
                 </div>
-            </button>
-        </Link>
+                <p className="sm:text-left text-center w-full">{description}</p>
+            </div>
+        </div>
     );
 };
 
