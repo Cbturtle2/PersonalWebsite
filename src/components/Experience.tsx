d242623cce1c8ef3d92dcae06584bec234163b25
Charles Beam <charleslbeam@gmail.com>
2024-08-23 00:54:18 -0500

    Made experience logos bigger

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -6,10 +6,16 @@ interface ExperienceProps extends React.HTMLAttributes<HTMLButtonElement> {
     description?: string | string[];
 }
 
-const Experience = ({ image, title, jobTitle, dates, description }: ExperienceProps) => {
+const Experience = ({
+    image,
+    title,
+    jobTitle,
+    dates,
+    description,
+}: ExperienceProps) => {
     return (
         <div className="flex gap-3 ml-3 w-full mb-8">
-            <img src={image} className="rounded-lg w-16 h-16" />
+            <img src={image} className="rounded-lg w-24 h-24" />
             <div className="flex flex-col items-start w-full">
                 <div className="flex justify-between w-full gap-3">
                     <h3 className="font-bold w-1/3 text-center">{title}</h3>
@@ -19,9 +25,15 @@ const Experience = ({ image, title, jobTitle, dates, description }: ExperiencePr
                     <h4 className="font-bold 1-1/3 text-center">{dates}</h4>
                 </div>
                 {Array.isArray(description) ? (
-                    description.map((line) => <p className="mt-4 sm:text-left text-center w-full">{line}</p>)
+                    description.map((line) => (
+                        <p className="mt-4 sm:text-left text-center w-full">
+                            {line}
+                        </p>
+                    ))
                 ) : (
-                    <p className="mt-4 sm:text-left text-center w-full">{description}</p>
+                    <p className="mt-4 sm:text-left text-center w-full">
+                        {description}
+                    </p>
                 )}
             </div>
         </div>
